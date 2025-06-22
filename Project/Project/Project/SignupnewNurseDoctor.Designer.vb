32a89fe5ad567dcbd8cf12721d65ff5c730c1bff
yloire <lancecunanan087@gmail.com>
2025-06-22 19:29:54 +0800

    Add files via upload

--- Project/Project/Project/SignupnewNurseDoctor.Designer.vb
+++ Project/Project/Project/SignupnewNurseDoctor.Designer.vb
@@ -22,6 +22,7 @@ Partial Class SignupnewNurseDoctor
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(SignupnewNurseDoctor))
         Me.ButtonCreate = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.TextBoxID = New System.Windows.Forms.TextBox()
@@ -34,136 +35,310 @@ Partial Class SignupnewNurseDoctor
         Me.Label5 = New System.Windows.Forms.Label()
         Me.LabelDepartment = New System.Windows.Forms.Label()
         Me.TextBoxDepartment = New System.Windows.Forms.TextBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.Panel5 = New System.Windows.Forms.Panel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        Me.Panel5.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'ButtonCreate
         '
-        Me.ButtonCreate.Location = New System.Drawing.Point(104, 233)
+        Me.ButtonCreate.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ButtonCreate.Location = New System.Drawing.Point(130, 578)
+        Me.ButtonCreate.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ButtonCreate.Name = "ButtonCreate"
-        Me.ButtonCreate.Size = New System.Drawing.Size(75, 23)
+        Me.ButtonCreate.Size = New System.Drawing.Size(249, 40)
         Me.ButtonCreate.TabIndex = 0
         Me.ButtonCreate.Text = "Create"
-        Me.ButtonCreate.UseVisualStyleBackColor = True
+        Me.ButtonCreate.UseVisualStyleBackColor = False
         '
         'Label1
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Segoe UI", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(70, 18)
+        Me.Label1.Location = New System.Drawing.Point(149, 184)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(150, 47)
+        Me.Label1.Size = New System.Drawing.Size(188, 60)
         Me.Label1.TabIndex = 1
         Me.Label1.Text = "Sign Up"
         '
         'TextBoxID
         '
-        Me.TextBoxID.Location = New System.Drawing.Point(123, 93)
+        Me.TextBoxID.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBoxID.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxID.Location = New System.Drawing.Point(66, 15)
+        Me.TextBoxID.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxID.Name = "TextBoxID"
-        Me.TextBoxID.Size = New System.Drawing.Size(121, 20)
+        Me.TextBoxID.Size = New System.Drawing.Size(172, 20)
         Me.TextBoxID.TabIndex = 2
         '
         'TextBoxAdminKey
         '
-        Me.TextBoxAdminKey.Location = New System.Drawing.Point(123, 197)
+        Me.TextBoxAdminKey.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBoxAdminKey.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxAdminKey.Location = New System.Drawing.Point(66, 19)
+        Me.TextBoxAdminKey.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxAdminKey.Name = "TextBoxAdminKey"
-        Me.TextBoxAdminKey.Size = New System.Drawing.Size(121, 20)
+        Me.TextBoxAdminKey.Size = New System.Drawing.Size(172, 20)
         Me.TextBoxAdminKey.TabIndex = 3
         Me.TextBoxAdminKey.UseSystemPasswordChar = True
         Me.TextBoxAdminKey.Visible = False
         '
         'TextBoxPass
         '
-        Me.TextBoxPass.Location = New System.Drawing.Point(123, 119)
+        Me.TextBoxPass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBoxPass.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxPass.Location = New System.Drawing.Point(66, 15)
+        Me.TextBoxPass.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxPass.Name = "TextBoxPass"
-        Me.TextBoxPass.Size = New System.Drawing.Size(121, 20)
+        Me.TextBoxPass.Size = New System.Drawing.Size(172, 20)
         Me.TextBoxPass.TabIndex = 4
         Me.TextBoxPass.UseSystemPasswordChar = True
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(46, 96)
+        Me.Label2.Location = New System.Drawing.Point(49, 261)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.Size = New System.Drawing.Size(74, 16)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "ID Number:"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(51, 122)
+        Me.Label3.Location = New System.Drawing.Point(53, 325)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(56, 13)
+        Me.Label3.Size = New System.Drawing.Size(70, 16)
         Me.Label3.TabIndex = 6
         Me.Label3.Text = "Password:"
         '
         'LabelAdminKey
         '
         Me.LabelAdminKey.AutoSize = True
-        Me.LabelAdminKey.Location = New System.Drawing.Point(19, 200)
+        Me.LabelAdminKey.Location = New System.Drawing.Point(12, 514)
+        Me.LabelAdminKey.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelAdminKey.Name = "LabelAdminKey"
-        Me.LabelAdminKey.Size = New System.Drawing.Size(88, 13)
+        Me.LabelAdminKey.Size = New System.Drawing.Size(111, 16)
         Me.LabelAdminKey.TabIndex = 7
         Me.LabelAdminKey.Text = "Admin Password:"
         Me.LabelAdminKey.Visible = False
         '
         'ComboBoxRole
         '
+        Me.ComboBoxRole.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ComboBoxRole.ForeColor = System.Drawing.SystemColors.WindowText
         Me.ComboBoxRole.FormattingEnabled = True
-        Me.ComboBoxRole.Location = New System.Drawing.Point(123, 145)
+        Me.ComboBoxRole.Location = New System.Drawing.Point(66, 16)
+        Me.ComboBoxRole.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.ComboBoxRole.Name = "ComboBoxRole"
-        Me.ComboBoxRole.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBoxRole.Size = New System.Drawing.Size(172, 28)
         Me.ComboBoxRole.TabIndex = 8
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(75, 148)
+        Me.Label5.Location = New System.Drawing.Point(84, 388)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(32, 13)
+        Me.Label5.Size = New System.Drawing.Size(39, 16)
         Me.Label5.TabIndex = 9
         Me.Label5.Text = "Role:"
         '
         'LabelDepartment
         '
         Me.LabelDepartment.AutoSize = True
-        Me.LabelDepartment.Location = New System.Drawing.Point(42, 175)
+        Me.LabelDepartment.Location = New System.Drawing.Point(43, 451)
+        Me.LabelDepartment.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelDepartment.Name = "LabelDepartment"
-        Me.LabelDepartment.Size = New System.Drawing.Size(65, 13)
+        Me.LabelDepartment.Size = New System.Drawing.Size(80, 16)
         Me.LabelDepartment.TabIndex = 11
         Me.LabelDepartment.Text = "Department:"
         Me.LabelDepartment.Visible = False
         '
         'TextBoxDepartment
         '
-        Me.TextBoxDepartment.Location = New System.Drawing.Point(123, 172)
+        Me.TextBoxDepartment.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.TextBoxDepartment.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBoxDepartment.Location = New System.Drawing.Point(66, 15)
+        Me.TextBoxDepartment.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxDepartment.Name = "TextBoxDepartment"
-        Me.TextBoxDepartment.Size = New System.Drawing.Size(121, 20)
+        Me.TextBoxDepartment.Size = New System.Drawing.Size(172, 20)
         Me.TextBoxDepartment.TabIndex = 10
         Me.TextBoxDepartment.UseSystemPasswordChar = True
         Me.TextBoxDepartment.Visible = False
         '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(159, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(156, 156)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.TextBoxID)
+        Me.Panel1.Location = New System.Drawing.Point(130, 261)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(249, 57)
+        Me.Panel1.TabIndex = 13
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel2.Controls.Add(Me.PictureBox3)
+        Me.Panel2.Controls.Add(Me.TextBoxPass)
+        Me.Panel2.Location = New System.Drawing.Point(130, 325)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(249, 57)
+        Me.Panel2.TabIndex = 14
+        '
+        'Panel3
+        '
+        Me.Panel3.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel3.Controls.Add(Me.PictureBox4)
+        Me.Panel3.Controls.Add(Me.ComboBoxRole)
+        Me.Panel3.Location = New System.Drawing.Point(130, 388)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(249, 57)
+        Me.Panel3.TabIndex = 15
+        '
+        'Panel4
+        '
+        Me.Panel4.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel4.Controls.Add(Me.PictureBox5)
+        Me.Panel4.Controls.Add(Me.TextBoxDepartment)
+        Me.Panel4.Location = New System.Drawing.Point(130, 451)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(249, 57)
+        Me.Panel4.TabIndex = 16
+        '
+        'Panel5
+        '
+        Me.Panel5.BackColor = System.Drawing.SystemColors.Window
+        Me.Panel5.Controls.Add(Me.PictureBox6)
+        Me.Panel5.Controls.Add(Me.TextBoxAdminKey)
+        Me.Panel5.Location = New System.Drawing.Point(130, 514)
+        Me.Panel5.Name = "Panel5"
+        Me.Panel5.Size = New System.Drawing.Size(249, 57)
+        Me.Panel5.TabIndex = 17
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(56, 51)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 18
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(56, 51)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 19
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Image = CType(resources.GetObject("PictureBox4.Image"), System.Drawing.Image)
+        Me.PictureBox4.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(56, 51)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 19
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Image = CType(resources.GetObject("PictureBox5.Image"), System.Drawing.Image)
+        Me.PictureBox5.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(56, 51)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 19
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Image = CType(resources.GetObject("PictureBox6.Image"), System.Drawing.Image)
+        Me.PictureBox6.Location = New System.Drawing.Point(3, 3)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(56, 51)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 19
+        Me.PictureBox6.TabStop = False
+        '
         'SignupnewNurseDoctor
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
-        Me.ClientSize = New System.Drawing.Size(269, 268)
-        Me.Controls.Add(Me.LabelDepartment)
-        Me.Controls.Add(Me.TextBoxDepartment)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.ComboBoxRole)
-        Me.Controls.Add(Me.LabelAdminKey)
-        Me.Controls.Add(Me.Label3)
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(485, 644)
         Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.TextBoxPass)
-        Me.Controls.Add(Me.TextBoxAdminKey)
-        Me.Controls.Add(Me.TextBoxID)
+        Me.Controls.Add(Me.LabelAdminKey)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.LabelDepartment)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.ButtonCreate)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel5)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
         Me.Name = "SignupnewNurseDoctor"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "SignupnewNurseDoctor"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel4.ResumeLayout(False)
+        Me.Panel4.PerformLayout()
+        Me.Panel5.ResumeLayout(False)
+        Me.Panel5.PerformLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -181,4 +356,15 @@ Partial Class SignupnewNurseDoctor
     Friend WithEvents Label5 As Label
     Friend WithEvents LabelDepartment As Label
     Friend WithEvents TextBoxDepartment As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents Panel5 As Panel
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents PictureBox6 As PictureBox
 End Class
